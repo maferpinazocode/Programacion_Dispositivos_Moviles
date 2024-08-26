2c9b9bf6f6ab05b1cbbdfc82618d0d324584452b
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-08-25 21:59:20 -0500

    Agregando menú principal de Piedra, Papel o Tijera

--- Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_2/Main_2.kt
+++ Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_2/Main_2.kt
@@ -1,2 +1,16 @@
 package Ejercicio_2
 
+import kotlin.random.Random
+
+fun main(args: Array<String>) {
+    println("Elegir la opción que desea (un número):")
+    println("1 -> Piedra")
+    println("2 -> Papel")
+    println("3 -> Tijera")
+
+    var opcionUsuario = readln().toInt()
+    var opcionGenerada = Random.nextInt(1,4)
+
+    println("La opción del computador es $opcionGenerada")
+
+}
