d53a4068e4bcb81dacb8a1348657e9f4096404d8
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-08-25 22:34:28 -0500

    Modularizamos el código, de tal manera que dividimos las funciones de dar pistas, crear números aleatorios, iniciar juego, etc.

--- Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_3/Main_3.kt
+++ Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_3/Main_3.kt
@@ -3,32 +3,60 @@ package Ejercicio_3
 import kotlin.random.Random
 
 fun main(args: Array<String>) {
-    val numeroGenerado = Random.nextInt(1, 31)  // O usar 32 si quieres incluir el 31.
-    var numIntentos: Int = 5
+    val numeroGenerado = crearNumeroAleatorio(1, 31)
+    val numIntentos = 5
     println("-- Bienvenido a adivinar el número --")
     println("Se te otorgarán pistas ")
 
-    while (numIntentos > 0) {
-        println("Te quedan $numIntentos intento(s).")
+    ejecutarJuego(numeroGenerado, numIntentos)
+}
+
+fun crearNumeroAleatorio(min: Int, max: Int): Int {
+    return Random.nextInt(min, max)
+}
+
+fun ejecutarJuego(numeroGenerado: Int, intentosRestantes: Int) {
+    var intentos = intentosRestantes
+
+    while (intentos > 0) {
+        println("Te quedan $intentos intento(s).")
         println("¡Ingresa el número!")
-        val numAdivinar = readln().toInt()
+        val numeroIngresado = readln().toIntOrNull()
 
-        if (numAdivinar < numeroGenerado) {
-            println("¡El número a adivinar es mayor!")
-            println("Vuelve a intentarlo.")
-        } else if (numAdivinar > numeroGenerado) {
-            println("¡El número a adivinar es menor!")
-            println("Vuelve a intentarlo.")
+        if (numeroIngresado != null) {
+            if (verificarNumero(numeroIngresado, numeroGenerado)) {
+                println("¡Muy bien, acertaste :)")
+                break
+            } else {
+                generarPista(numeroIngresado, numeroGenerado)
+                intentos -= 1
+                if (intentos > 0) {
+                    println("Vuelve a intentarlo.")
+                }
+            }
         } else {
-            println("¡El número a adivinar es $numeroGenerado!")
-            println("Muy bien, acertaste :)")
-            break
+            println("Por favor, ingresa un número válido.")
         }
-
-        numIntentos -= 1
     }
 
-    if (numIntentos == 0) {
+    if (intentos == 0) {
         println("Lo siento, se te acabaron los intentos. El número era $numeroGenerado.")
+    }
+}
+
+fun verificarNumero(numeroIngresado: Int, numeroGenerado: Int): Boolean {
+    return if (numeroIngresado == numeroGenerado) {
+        println("¡El número a adivinar es $numeroGenerado!")
+        true
+    } else {
+        false
+    }
+}
+
+fun generarPista(numeroIngresado: Int, numeroGenerado: Int) {
+    if (numeroIngresado < numeroGenerado) {
+        println("¡El número a adivinar es mayor!")
+    } else {
+        println("¡El número a adivinar es menor!")
     }
 }
