0c4d89fb1c7c4ac6059adf50677e1a66bdbd6617
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-08-25 22:08:06 -0500

    Añadimos el manejo de excepciones por si se ingresa un String, en vez de un número, de igual manera si es que la opción ingresada no es válida.

--- Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_2/Main_2.kt
+++ Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_2/Main_2.kt
@@ -17,9 +17,15 @@ fun obtenerOpcionUsuario(): Int {
     println("2 -> Papel")
     println("3 -> Tijera")
 
-    val opcion = readln().toInt()
-
-    return opcion
+    try {
+        val opcion = readln().toInt()
+        if (opcion !in 1..3) {
+            throw IllegalArgumentException("Opción no válida. Debe ser 1, 2 o 3.")
+        }
+        return opcion
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("Debe ingresar un número válido.")
+    }
 }
 
 fun main(args: Array<String>) {
