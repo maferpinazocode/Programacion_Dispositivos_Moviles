5e83f4e7ac7617041f8c0d57009762dcc276d97c
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-08-25 22:26:10 -0500

    Cambiamos el bucle, ahora se ejecuta mientras el numIntentos sea mayor que 0.

--- Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_3/Main_3.kt
+++ Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_3/Main_3.kt
@@ -2,29 +2,33 @@ package Ejercicio_3
 
 import kotlin.random.Random
 
-fun main(args: Array<String>){
-    var numeroGenerado = Random.nextInt(1,31)
+fun main(args: Array<String>) {
+    val numeroGenerado = Random.nextInt(1, 31)  // O usar 32 si quieres incluir el 31.
     var numIntentos: Int = 5
     println("-- Bienvenido a adivinar el número --")
-    println("Se te otorgaran pistas ")
+    println("Se te otorgarán pistas ")
 
-    while (numIntentos <= 5) {
-        println("Solo tienes $numIntentos intentos ")
+    while (numIntentos > 0) {
+        println("Te quedan $numIntentos intento(s).")
         println("¡Ingresa el número!")
-        var numAdivinar = readln().toInt()
-        if (numAdivinar < numeroGenerado){
+        val numAdivinar = readln().toInt()
+
+        if (numAdivinar < numeroGenerado) {
             println("¡El número a adivinar es mayor!")
-            println("Vuelve a intentarlo")
-        }
-        else if(numAdivinar > numeroGenerado){
+            println("Vuelve a intentarlo.")
+        } else if (numAdivinar > numeroGenerado) {
             println("¡El número a adivinar es menor!")
-            println("Vuelve a intentarlo")
-        }
-        else{
+            println("Vuelve a intentarlo.")
+        } else {
             println("¡El número a adivinar es $numeroGenerado!")
             println("Muy bien, acertaste :)")
             break
         }
-        numIntentos = numIntentos - 1
+
+        numIntentos -= 1
+    }
+
+    if (numIntentos == 0) {
+        println("Lo siento, se te acabaron los intentos. El número era $numeroGenerado.")
     }
 }
