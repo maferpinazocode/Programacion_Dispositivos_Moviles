9ff3166dc7b5122aed9dc2d7e60aa6cdf9e55ccb
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-08-25 22:15:50 -0500

    Continuamos con la lógica para dar pistas sobre el número que se debe adivinar, con manejo de if, de la misma manera imprimimos mensajes (pistas)

--- Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_3/Main_3.kt
+++ Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_3/Main_3.kt
@@ -8,4 +8,23 @@ fun main(args: Array<String>){
     println("-- Bienvenido a adivinar el número --")
     println("Se te otorgaran pistas ")
 
+    while (numIntentos <= 5) {
+        println("Solo tienes $numIntentos intentos ")
+        println("¡Ingresa el número!")
+        var numAdivinar = readln().toInt()
+        if (numAdivinar < numeroGenerado){
+            println("¡El número a adivinar es mayor!")
+            println("Vuelve a intentarlo")
+        }
+        else if(numAdivinar > numeroGenerado){
+            println("¡El número a adivinar es menor!")
+            println("Vuelve a intentarlo")
+        }
+        else{
+            println("¡El número a adivinar es $numeroGenerado!")
+            println("Muy bien, acertaste :)")
+            break
+        }
+        numIntentos = numIntentos - 1
+    }
 }
