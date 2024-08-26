c2934d59252b231e1cab542b40765f2765392f9d
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-08-25 22:43:11 -0500

    Seguimos con la creación de las variables para cada número, de la misma manera, usamos when para hallar que opción quiere realizar el usuario.

--- Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_4/Main_4.kt
+++ Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_4/Main_4.kt
@@ -11,9 +11,23 @@ fun main (args: Array<String>){
         println("5. Salir")
 
         opcion = readln().toInt()
-        if (opcion == 5) {
+        if (opcion == 5){
             return
         }
 
+        var numero1: Double = readln().toDouble()
+        var numero2: Double = readln().toDouble()
+        // POR QUE ME CORTAS?
+        // SE CORTO SOLO INTERNET FALLO
+
+        var respuesta = when (opcion) {
+            1 -> numero1 + numero2
+            2 -> numero1 - numero2
+            3 -> numero1 * numero2
+            4 -> numero1 / numero2
+            else -> "Error"
+        }
+
+        println("Resultado: $respuesta")
     }
 }
