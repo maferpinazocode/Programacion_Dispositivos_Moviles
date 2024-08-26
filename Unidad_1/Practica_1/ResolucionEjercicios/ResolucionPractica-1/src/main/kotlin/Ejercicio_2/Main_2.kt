1299cad2f5a2620227ed43a420dc6f515b9678a4
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-08-25 22:01:52 -0500

    Agregando función para hallar el ganador, utilizamos if y when para identificar al ganador, o si es que existe empate, al final, imprimimos al ganador

--- Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_2/Main_2.kt
+++ Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_2/Main_2.kt
@@ -2,6 +2,29 @@ package Ejercicio_2
 
 import kotlin.random.Random
 
+fun encontrarGanador(opUsuario: Int, opComputador: Int): String {
+    var nGanador: Int = 0
+    var opEmpate: Int = 4
+
+    if (opUsuario == opComputador){
+        nGanador = opEmpate
+    }
+    else if ((opUsuario == 1 && opComputador == 3) || (opUsuario == 2 && opComputador == 1) || (opUsuario == 3 && opComputador == 2)){
+        nGanador = opUsuario
+    }
+    else {
+        nGanador = opComputador
+    }
+
+    var ganador = when (nGanador){
+        opComputador -> "Computador :("
+        opUsuario -> "Usuario :)"
+        opEmpate -> "Hubo Empate :o"
+        else -> "Error"
+    }
+
+    return ganador
+}
 fun main(args: Array<String>) {
     println("Elegir la opción que desea (un número):")
     println("1 -> Piedra")
@@ -13,4 +36,6 @@ fun main(args: Array<String>) {
 
     println("La opción del computador es $opcionGenerada")
 
+    var ganador = encontrarGanador(opcionUsuario, opcionGenerada)
+    println("El ganador es: \n$ganador")
 }
