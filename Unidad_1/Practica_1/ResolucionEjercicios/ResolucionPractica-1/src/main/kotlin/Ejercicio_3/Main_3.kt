e609ceaffc598c0c6c2b4a0f334ea02269137c95
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-08-25 22:14:38 -0500

    Iniciamos con el ejercicio 3, creamos la variable para el numero generado importando Random, de la misma manera, definimos los 5 intentos

--- Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_3/Main_3.kt
+++ Unidad_1/Practica_1/ResolucionEjercicios/ResolucionPractica-1/src/main/kotlin/Ejercicio_3/Main_3.kt
@@ -1,2 +1,11 @@
 package Ejercicio_3
 
+import kotlin.random.Random
+
+fun main(args: Array<String>){
+    var numeroGenerado = Random.nextInt(1,31)
+    var numIntentos: Int = 5
+    println("-- Bienvenido a adivinar el número --")
+    println("Se te otorgaran pistas ")
+
+}
