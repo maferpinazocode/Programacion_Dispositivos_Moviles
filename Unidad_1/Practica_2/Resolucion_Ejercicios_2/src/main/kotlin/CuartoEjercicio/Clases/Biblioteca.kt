48ffb4c75259fc046f0eede67c1e5ca2df15b259
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-09-01 14:20:32 -0500

    En la clase Biblioteca, implementamos la lógica para cada función de la interfaz IBiblioteca, para prestar material a usuarios, mostrar los materiales disponibles, los materiales que son reservados por un usuario, etc.

--- Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/CuartoEjercicio/Clases/Biblioteca.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/CuartoEjercicio/Clases/Biblioteca.kt
@@ -8,25 +8,44 @@ class Biblioteca : IBiblioteca {
     private val usuarios = mutableMapOf<Usuario, MutableList<Material>>()
 
     override fun registrarMaterial(material: Material) {
+        materialesDisponibles.add(material)
     }
 
     override fun registrarUsuario(usuario: Usuario) {
+        usuarios[usuario] = mutableListOf()
     }
 
     override fun prestarMaterial(usuario: Usuario, material: Material): Boolean {
-
+        if (materialesDisponibles.contains(material) && usuarios.containsKey(usuario)) {
+            materialesDisponibles.remove(material)
+            usuarios[usuario]?.add(material)
+            return true
+        }
         return false
     }
 
     override fun devolverMaterial(usuario: Usuario, material: Material): Boolean {
-        return false 
+        if (usuarios[usuario]?.contains(material) == true) {
+            usuarios[usuario]?.remove(material)
+            materialesDisponibles.add(material)
+            return true
+        }
+        return false
     }
 
     override fun mostrarMaterialesDisponibles() {
-
+        println("Materiales Disponibles:")
+        for (material in materialesDisponibles) {
+            material.mostrarDetalles()
+            println()
+        }
     }
 
     override fun mostrarMaterialesReservadosPorUsuario(usuario: Usuario) {
-
+        println("Materiales Reservados por ${usuario.nombre} ${usuario.apellido}:")
+        usuarios[usuario]?.forEach { material ->
+            material.mostrarDetalles()
+            println()
+        }
     }
 }
