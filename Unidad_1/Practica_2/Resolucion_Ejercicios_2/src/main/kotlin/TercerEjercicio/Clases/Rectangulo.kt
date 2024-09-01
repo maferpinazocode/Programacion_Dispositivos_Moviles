d1c9f783de8a971a03a6a0e4072c76fb3279ed8d
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-09-01 12:51:34 -0500

    En la clase rectangulo, añadimos las funciones que hereda de Shape, de la misma forma, implementamos la formula. Agregamos al main, la creación de cada una de las figuras con sus respectivos lados, e imprimimos.

--- Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/TercerEjercicio/Clases/Rectangulo.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/TercerEjercicio/Clases/Rectangulo.kt
@@ -1,4 +1,23 @@
 package TercerEjercicio.Clases
 
-class Rectangulo {
+class Rectangulo(private val largo: Double, private val ancho: Double) : Shape() {
+    override var area: Double = 0.0
+         set
+    override var perimetro: Double = 0.0
+         set
+
+    init {
+        hallarArea()
+        hallarPerimetro()
+    }
+
+    override fun hallarArea() {
+        area = largo * ancho
+    }
+
+    override fun hallarPerimetro() {
+        perimetro = 2 * (largo + ancho)
+    }
+
+
 }
--- Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/TercerEjercicio/Controlador/Main_3.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/TercerEjercicio/Controlador/Main_3.kt
@@ -1,2 +1,20 @@
 package TercerEjercicio.Controlador
 
+import TercerEjercicio.Clases.Circulo
+import TercerEjercicio.Clases.Cuadrado
+import TercerEjercicio.Clases.Rectangulo
+
+fun main() {
+    val cuadrado = Cuadrado(6.0)
+    val circulo = Circulo(4.0)
+    val rectangulo = Rectangulo(9.0, 5.0)
+
+    println("Cuadrado:")
+    cuadrado.imprimirInformacion()
+
+    println("\nCírculo:")
+    circulo.imprimirInformacion()
+
+    println("\nRectángulo:")
+    rectangulo.imprimirInformacion()
+}
