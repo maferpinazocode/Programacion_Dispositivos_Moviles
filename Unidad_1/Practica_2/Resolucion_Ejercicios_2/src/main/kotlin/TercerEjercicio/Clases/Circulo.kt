4cfa839b76dec9ee7ef7806ba9d28c7fb82b5a12
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-09-01 12:32:29 -0500

    En la clase cuadrado, creamos el atributo lado, sobreescribimos las funciones que hereda de la clase abstracta 'Shape', y hacemos un Init, para inicializar las funciones. De igual manera con la clase Circulo, pero en vez de tener como atributo lado, tiene radio.

--- Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/TercerEjercicio/Clases/Circulo.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/TercerEjercicio/Clases/Circulo.kt
@@ -1,4 +1,24 @@
 package TercerEjercicio.Clases
 
-class Circulo {
+class Circulo(private val radio: Double) : Shape() {
+    override var area: Double = 0.0
+         set
+    override var perimetro: Double = 0.0
+         set
+
+    init {
+        hallarArea()
+        hallarPerimetro()
+    }
+
+    override fun hallarArea() {
+        area = Math.PI * radio * radio
+    }
+
+    override fun hallarPerimetro() {
+        perimetro = 2 * Math.PI * radio
+    }
+
+
+
 }
--- Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/TercerEjercicio/Clases/Cuadrado.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/TercerEjercicio/Clases/Cuadrado.kt
@@ -1,4 +1,21 @@
 package TercerEjercicio.Clases
 
-class Cuadrado {
+class Cuadrado(private val lado: Double) : Shape() {
+    override var area: Double = 0.0
+        set
+    override var perimetro: Double = 0.0
+        set
+
+    init {
+        hallarArea()
+        hallarPerimetro()
+    }
+
+    override fun hallarArea() {
+        area = lado * lado
+    }
+
+    override fun hallarPerimetro() {
+        perimetro = 4 * lado
+    }
 }
