355fd86bea48f5c4d56c3317d2d3725daa031654
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-09-01 10:18:44 -0500

    Creamos la variable descuento, de la misma manera el setter se asegura que el descuento este en un rango de 0 y 100 si es que es modificado, y el get, se encarga de mostrar las veces que se ha accedido a un descuento el producto.

--- Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/SegundoEjercicio/Clases/Producto.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/SegundoEjercicio/Clases/Producto.kt
@@ -1,9 +1,11 @@
 package SegundoEjercicio.Clases
 
-class Producto {
+class Producto(precio: Double, descuento: Double) {
     var preciosAccesos: Int = 0
+    var descuentoAccesos: Int = 0
 
-    var precio: Double = 0.0
+
+    var precio: Double = precio
         set(value) {
             if (value >= 0){
                 field = value
@@ -15,6 +17,21 @@ class Producto {
         get() {
             preciosAccesos ++
             println("Accediendo al precio. Veces accedido: $preciosAccesos")
+            return field
+        }
+
+    var descuento: Double = descuento
+        set(value) {
+            if (value in 0.0 .. 100.0){
+                field = value
+            }
+            else{
+                throw IllegalArgumentException("El descuento no puede ser negativo. De estar entre 0 y 100.")
+            }
+        }
+        get() {
+            descuentoAccesos++
+            println("Accediendo al descuento. Veces accedido: $descuentoAccesos")
             return field
         }
 }
