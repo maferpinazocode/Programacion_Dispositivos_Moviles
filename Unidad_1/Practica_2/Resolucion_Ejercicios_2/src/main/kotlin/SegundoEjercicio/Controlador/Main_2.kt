7bcf9a279485b3a71d96884ec1f8c33cea01f40a
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-09-01 10:40:34 -0500

    En la función main, creamos un valor producto, y probamos sus metodos correspondientes.

--- Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/SegundoEjercicio/Controlador/Main_2.kt
+++ Unidad_1/Practica_2/Resolucion_Ejercicios_2/src/main/kotlin/SegundoEjercicio/Controlador/Main_2.kt
@@ -1,2 +1,18 @@
 package SegundoEjercicio.Controlador
 
+import SegundoEjercicio.Clases.Producto
+
+fun main() {
+    val producto = Producto(100.0,15.0)
+
+
+    println("Precio original: ${producto.precio}")
+    println("Descuento aplicado: ${producto.descuento}%")
+    println("Precio final: ${producto.calcularPrecioFinal()}")
+
+    println("Precio original: ${producto.precio}")
+    println("Descuento aplicado: ${producto.descuento}%")
+
+    producto.reiniciarProducto()
+    println("Producto reiniciado. Precio: ${producto.precio}, Descuento: ${producto.descuento}")
+}
