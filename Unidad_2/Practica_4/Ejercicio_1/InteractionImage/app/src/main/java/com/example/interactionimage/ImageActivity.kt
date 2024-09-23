97fe74aaed7a43fd740a5806e700b34301a36d89
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-09-22 23:07:39 -0500

    Copiamos la estructura del primer ejercicio, para empezar a trabajar en el. Validamos que funcione correctamente primero.

---
+++ Unidad_2/Practica_4/Ejercicio_1/InteractionImage/app/src/main/java/com/example/interactionimage/ImageActivity.kt
@@ -0,0 +1,33 @@
+package com.example.interactionimage
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class ImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+
+        // Obtener la referencia del ImageView
+        val imageView: ImageView = findViewById(R.id.imageView)
+
+        // Obtener el índice de la imagen seleccionada desde el Intent
+        val imageIndex = intent.getIntExtra("image_index", 0)
+
+        // Mostrar la imagen correspondiente
+        when (imageIndex) {
+            0 -> imageView.setImageResource(R.drawable.cocker1)
+            1 -> imageView.setImageResource(R.drawable.hushpuppy1)
+            2 -> imageView.setImageResource(R.drawable.husky1)
+        }
+
+        // Configurar el botón para volver a la actividad anterior
+        val backButton: Button = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            finish() // Termina la actividad actual y vuelve a la anterior
+        }
+    }
+}
