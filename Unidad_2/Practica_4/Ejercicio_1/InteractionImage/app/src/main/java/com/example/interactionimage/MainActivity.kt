e60c51c5c00fb41dd152ced4d2b7bcb5ade52e2b
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-09-22 23:38:46 -0500

    Al crear los archivos Fragment, en MainAcitvity principal, cargamos el fragment de selección de imagen inicial.

--- Unidad_2/Practica_4/Ejercicio_1/InteractionImage/app/src/main/java/com/example/interactionimage/MainActivity.kt
+++ Unidad_2/Practica_4/Ejercicio_1/InteractionImage/app/src/main/java/com/example/interactionimage/MainActivity.kt
@@ -1,46 +1,19 @@
 package com.example.interactionimage
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var imageSpinner: Spinner
-    private lateinit var nextButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Referencias a Spinner y Button
-        imageSpinner = findViewById(R.id.imageSpinner)
-        nextButton = findViewById(R.id.nextButton)
-
-        // Crear un array de nombres de las imágenes
-        val imageNames = arrayOf("Cocker Spaniel", "Basset Hound", "Husky")
-
-        // Crear un adaptador para el Spinner
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, imageNames)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        imageSpinner.adapter = adapter
-
-        // Listener del botón para ir a la siguiente actividad
-        nextButton.setOnClickListener {
-            // Obtener el índice seleccionado en el Spinner
-            val selectedImageIndex = imageSpinner.selectedItemPosition
-
-            // Crear un Intent para cambiar de actividad
-            val intent = Intent(this, ImageActivity::class.java)
-
-            // Pasar el índice de la imagen seleccionada a la siguiente actividad
-            intent.putExtra("image_index", selectedImageIndex)
-
-            // Iniciar la siguiente actividad
-            startActivity(intent)
+        // Cargar el fragmento de selección de imagen al iniciar la actividad
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ImageSelectionFragment())
+                .commit()
         }
     }
 }
