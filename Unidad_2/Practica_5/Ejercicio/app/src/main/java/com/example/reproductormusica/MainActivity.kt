b9c54d572354167f9f62ced3dfb7724e47b89642
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-10-01 23:06:19 -0500

    Faltaba arreglar el MainAcitivity y su XML, entonces, añadimos el contenedor de fragmentos, el inicial AudioListFragment en el contenedor cuando se incie la aplicación.

--- Unidad_2/Practica_5/Ejercicio/app/src/main/java/com/example/reproductormusica/MainActivity.kt
+++ Unidad_2/Practica_5/Ejercicio/app/src/main/java/com/example/reproductormusica/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.reproductormusica
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Verificar si hay fragmentos ya añadidos (para manejar giros de pantalla)
+        if (savedInstanceState == null) {
+            // Iniciar el AudioListFragment en el contenedor
+            replaceFragment(AudioListFragment())
         }
+    }
+
+    // Función auxiliar para reemplazar fragmentos
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
